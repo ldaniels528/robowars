59b094dac2f920153096231a4ab1c8cbfb9e3c38
ldaniels <lawrence.daniels@topcareife.com>
2014-08-02 17:36:28 -0700

    streamlining events

--- src/main/scala/com/ldaniels528/robowars/events/EventSteeringCommand.scala
+++ src/main/scala/com/ldaniels528/robowars/events/EventSteeringCommand.scala
@@ -6,4 +6,4 @@ import com.ldaniels528.fxcore3d.FxEvent
  * Represents a vehicle steering command/event
  * @author lawrence.daniels@gmail.com
  */
-case class EventSteeringCommand(myTime: Double, command: Int, factor: Double, dt: Double) extends FxEvent
+case class EventSteeringCommand(time: Double, command: Int, factor: Double, dt: Double) extends FxEvent
