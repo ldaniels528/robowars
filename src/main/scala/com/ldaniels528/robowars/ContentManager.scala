12e9e012de570929b7c9c41d360d053c64c720da
ldaniels <lawrence.daniels@topcareife.com>
2014-08-06 16:37:32 -0700

    added image and virtual world loading

--- src/main/scala/com/ldaniels528/robowars/ContentManager.scala
+++ src/main/scala/com/ldaniels528/robowars/ContentManager.scala
@@ -1,6 +1,8 @@
 package com.ldaniels528.robowars
 
+import java.awt.image.BufferedImage
 import java.io._
+import javax.imageio.ImageIO
 
 import com.ldaniels528.fxcore3d.polygon.{FxConvexPolyhedron, FxPolyhedronLoader}
 
@@ -11,6 +13,19 @@ import com.ldaniels528.fxcore3d.polygon.{FxConvexPolyhedron, FxPolyhedronLoader}
 object ContentManager {
 
   /**
+   * Retrieves an image from the classpath
+   * @param path the given resource path
+   * @return a [[BufferedImage]]
+   */
+  def loadImage(path: String): BufferedImage = {
+    Option(getClass.getResource(path)) match {
+      case Some(url) => ImageIO.read(url)
+      case None =>
+        throw new IllegalStateException(s"Image '$path' not found")
+    }
+  }
+
+  /**
    * Loads a model from the class path
    * @param path the resource path of the model
    */
@@ -19,10 +34,20 @@ object ContentManager {
     FxPolyhedronLoader.readConvexPolyhedron(getResource(path))
   }
 
-  def loadWorld(path: String) = {
-
+  /**
+   * Retrieves a virtual model
+   * @param path the given resource path
+   * @return a [[VirtualWorld]]
+   */
+  def loadWorld(path: String): VirtualWorld = {
+    VirtualWorldReader.load(path)
   }
 
+  /**
+   * Returns an input stream representing the data of the given resource path
+   * @param path the given resource path
+   * @return an [[InputStream]]
+   */
   def getResource(path: String): InputStream = {
     Option(getClass.getResource(path)) match {
       case Some(url) => url.openStream()
