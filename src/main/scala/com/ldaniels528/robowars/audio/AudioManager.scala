c02eaeef7e9da0c6a799116a0972466bb2ca017e
ldaniels <lawrence.daniels@topcareife.com>
2014-08-06 21:51:42 -0700

    added new audio clips

--- src/main/scala/com/ldaniels528/robowars/audio/AudioManager.scala
+++ src/main/scala/com/ldaniels528/robowars/audio/AudioManager.scala
@@ -30,16 +30,17 @@ object AudioManager extends FxAudioPlayer {
    */
   private def loadSamples(): Map[AudioKey, FxAudioSample] = {
     val samples = Seq[(AudioKey, String)](
-      (BigExplosionClip -> "environmental/bigExplosion"),
-      (BuildingExplodeClip -> "environmental/buildingExplode"),
-      (CrashClip -> "environmental/crash"),
-      (ExplosionClip -> "environmental/bigExplosion"),
-      (ReloadClip -> "weapons/loadClip"),
-      (Ambient -> "background/ambient"),
-      (MachineGunClip -> "weapons/machineGun"),
-      (MiniCannonClip -> "weapons/44"),
-      (MissileClip -> "weapons/missile"),
-      (RewardClip -> "items/reward"))
+      BigExplosionClip -> "environmental/bigExplosion",
+      BuildingExplodeClip -> "environmental/buildingExplode",
+      CrashClip -> "environmental/crash",
+      ExplosionClip -> "environmental/bigExplosion",
+      GameOver -> "alerts/gameOver",
+      ReloadClip -> "weapons/loadClip",
+      Ambient -> "background/ambient",
+      MachineGunClip -> "weapons/machineGun",
+      MiniCannonClip -> "weapons/44",
+      MissileClip -> "weapons/missile",
+      RewardClip -> "items/reward")
     Map(samples map {
       case (key, name) => (key, loadSample(s"/audio/$name.wav"))
     }: _*)
@@ -76,6 +77,7 @@ object AudioManager extends FxAudioPlayer {
   case object BigExplosionClip extends AudioKey
   case object CrashClip extends AudioKey
   case object ExplosionClip extends AudioKey
+  case object GameOver extends AudioKey
   case object MachineGunClip extends AudioKey
   case object MiniCannonClip extends AudioKey
   case object MissileClip extends AudioKey
