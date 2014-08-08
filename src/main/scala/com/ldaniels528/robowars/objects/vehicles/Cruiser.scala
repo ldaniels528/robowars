406c7b0e92d8375e62a426c8cc308c457ce71f1c
ldaniels <lawrence.daniels@topcareife.com>
2014-08-08 15:28:39 -0700

    added cruiser vehicle

---
+++ src/main/scala/com/ldaniels528/robowars/objects/vehicles/Cruiser.scala
@@ -0,0 +1,51 @@
+package com.ldaniels528.robowars.objects.vehicles
+
+import com.ldaniels528.fxcore3d.polygon.{FxPolyhedron, FxPolyhedronInstance}
+import com.ldaniels528.fxcore3d.{FxPoint3D, FxVelocityVector, FxWorld}
+import com.ldaniels528.robowars.ContentManager
+import com.ldaniels528.robowars.objects.vehicles.Cruiser._
+import com.ldaniels528.robowars.objects.weapons.{MachineGun, MiniCannon}
+
+/**
+ * Cruiser Vehicle
+ * @author lawrence.daniels@gmail.com
+ */
+class Cruiser(w: FxWorld, p: FxPoint3D)
+  extends AbstractVehicle(w, FxPoint3D(p.x, p.y + SCALE.y, p.z), FxVelocityVector(Math.PI, 0, 0), health = 5d) {
+
+  val turningRate: Double = 1.25d
+  val pitchRate: Double = 0
+  val acceleration: Double = 3d
+  val brakingRate: Double = 20d
+  val maxVelocity: Double = 40d
+  val climbRate: Double = 0
+  val decentRate: Double = 0
+  val pitchClimbRateFactor: Double = 0
+
+  // set the default polyhedron instance
+  lazy val modelInstance = new FxPolyhedronInstance(MODEL, SCALE)
+
+  // attach some weapons
+  this += MachineGun(this, FxPoint3D(0, p.y + SCALE.y, 0))
+  this += MiniCannon(this, FxPoint3D(0, SCALE.y, 0))
+  selectWeapon(0)
+
+  override def die() {
+    super.die()
+
+    // leave the carcass behind
+    new FesseTankRemains(world, this)
+    ()
+  }
+
+}
+
+/**
+ * Cruiser Companion Object
+ * @author lawrence.daniels@gmail.com
+ */
+object Cruiser {
+  val MODEL: FxPolyhedron = ContentManager.loadModel("/models/actors/cruiser.f3d")
+  val SCALE = FxPoint3D(1.50d, 0.75d, 2.00d)
+
+}
