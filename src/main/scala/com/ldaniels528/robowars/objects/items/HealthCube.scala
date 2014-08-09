5fbe6ea8a8c3b021a0eec457caba06c3ee20c818
ldaniels <lawrence.daniels@topcareife.com>
2014-08-09 08:15:15 -0700

    continued simplification of the implementation

--- src/main/scala/com/ldaniels528/robowars/objects/items/HealthCube.scala
+++ src/main/scala/com/ldaniels528/robowars/objects/items/HealthCube.scala
@@ -1,8 +1,7 @@
 package com.ldaniels528.robowars.objects.items
 
 import com.ldaniels528.fxcore3d._
-import com.ldaniels528.fxcore3d.polygon.{FxPolyhedron, FxPolyhedronInstance}
-import com.ldaniels528.robowars.ContentManager
+import com.ldaniels528.fxcore3d.polygon.FxModelInstance
 import com.ldaniels528.robowars.objects.items.HealthCube._
 
 /**
@@ -12,7 +11,7 @@ import com.ldaniels528.robowars.objects.items.HealthCube._
 case class HealthCube(w: FxWorld, p: FxPoint3D) extends AbstractRewardItem(w, FxPoint3D(p.x, p.y + SCALE.h, p.z)) {
 
   // set the default polyhedron instance
-  lazy val modelInstance = new FxPolyhedronInstance(MODEL, SCALE)
+  lazy val modelInstance = FxModelInstance("/models/items/cube.f3d", SCALE)
 
 }
 
@@ -21,7 +20,6 @@ case class HealthCube(w: FxWorld, p: FxPoint3D) extends AbstractRewardItem(w, Fx
  * @author lawrence.daniels@gmail.com
  */
 object HealthCube {
-  private val MODEL: FxPolyhedron = ContentManager.loadModel("/models/items/cube.f3d")
   private val SCALE = FxScale3D(1, 1, 1)
 
 }
