a7ed90bc545e64f3f309ca214fd6edf3222e1d54
ldaniels <lawrence.daniels@topcareife.com>
2014-08-08 17:42:26 -0700

    renamed

--- src/main/scala/com/ldaniels528/robowars/objects/vehicles/VehicleRemains.scala
+++ src/main/scala/com/ldaniels528/robowars/objects/vehicles/VehicleRemains.scala
@@ -1,16 +1,16 @@
 package com.ldaniels528.robowars.objects.vehicles
 
 import com.ldaniels528.fxcore3d.polygon.{FxPolyhedron, FxPolyhedronInstance}
-import com.ldaniels528.fxcore3d.{FxAngle3D, FxPoint3D, FxWorld}
+import com.ldaniels528.fxcore3d.{FxScale3D, FxAngle3D, FxPoint3D, FxWorld}
 import com.ldaniels528.robowars.ContentManager
 import com.ldaniels528.robowars.objects.structures.AbstractMovingScenery
-import com.ldaniels528.robowars.objects.vehicles.FesseTankRemains._
+import com.ldaniels528.robowars.objects.vehicles.VehicleRemains._
 
 /**
- * Remains of a Fesse Tank
+ * Remains of a Vehicle
  * @author lawrence.daniels@gmail.com
  */
-class FesseTankRemains(world: FxWorld, deadTank: AbstractVehicle)
+class VehicleRemains(world: FxWorld, deadTank: AbstractVehicle)
   extends AbstractMovingScenery(world, deadTank.position, deadTank.angle, deadTank.getdPosition(), deadTank.getdAngle()) {
 
   // set the default polyhedron instance
@@ -28,9 +28,9 @@ class FesseTankRemains(world: FxWorld, deadTank: AbstractVehicle)
     super.update(dt)
     val p = position
     // -- check if hit the ground
-    if (p.y < SCALE.y) {
+    if (p.y < SCALE.h) {
       val dp = getdPosition()
-      p.y = SCALE.y
+      p.y = SCALE.h
       dp.set(0, 0, 0)
       setPosition(p)
       val a = angle
@@ -38,7 +38,7 @@ class FesseTankRemains(world: FxWorld, deadTank: AbstractVehicle)
       setAngle(a)
       setdAngle(new FxAngle3D())
       setdPosition(dp)
-    } else if (p.y > SCALE.y) {
+    } else if (p.y > SCALE.h) {
       val dp = getdPosition()
       dp.y += world.gravity * dt
       setdPosition(dp)
@@ -47,12 +47,12 @@ class FesseTankRemains(world: FxWorld, deadTank: AbstractVehicle)
 }
 
 /**
- * FesseTankRemains Companion Object
+ * VehicleRemains Companion Object
  * @author lawrence.daniels@gmail.com
  */
-object FesseTankRemains {
+object VehicleRemains {
   val MODEL: FxPolyhedron = ContentManager.loadModel("/models/actors/fesseTankRemains.f3d")
-  val SCALE = new FxPoint3D(1.5d, 0.23d, 2d)
+  val SCALE = new FxScale3D(1.5d, 0.23d, 2d)
   val ourSpeedUp: Double = 20d
   val ourRandRot: Double = 3d
 
