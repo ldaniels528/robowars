5b453a0d5187164f79c2a0fbf45bec0f6e679ac7
ldaniels <lawrence.daniels@topcareife.com>
2014-08-10 10:41:04 -0700

    added

---
+++ src/main/scala/com/ldaniels528/robowars/objects/structures/Structure.scala
@@ -0,0 +1,7 @@
+package com.ldaniels528.robowars.objects.structures
+
+/**
+ * This trait is a marker to indicate that an object is a structure (buildings, walls, etc.)
+ * @author lawrence.daniels@gmail.com
+ */
+trait Structure
