dab850d932c21cb62d9e8dd778a6f75a8c62dab7
ldaniels <lawrence.daniels@topcareife.com>
2014-08-10 12:47:17 -0700

    renamed and modified to use the new moving door states enumeration

--- src/main/scala/com/ldaniels528/robowars/objects/structures/MovingDoor.scala
+++ src/main/scala/com/ldaniels528/robowars/objects/structures/MovingDoor.scala
@@ -1,21 +1,20 @@
-package com.ldaniels528.robowars.objects.structures.moving
+package com.ldaniels528.robowars.objects.structures
 
 import com.ldaniels528.fxcore3d._
-import com.ldaniels528.robowars.objects.structures.Structure
-import com.ldaniels528.robowars.objects.structures.moving.AbstractMovingDoor._
+import com.ldaniels528.robowars.objects.structures.MovingDoorStates._
 import com.ldaniels528.robowars.objects.vehicles.AbstractVehicle
 
 /**
- * Abstract Door
+ * Represents a Moving Door
  * @author lawrence.daniels@gmail.com
  */
-abstract class AbstractMovingDoor(world: FxWorld, pos: FxPoint3D, agl: FxAngle3D)
+abstract class MovingDoor(world: FxWorld, pos: FxPoint3D, agl: FxAngle3D)
   extends FxMovingObject(world, pos, agl, FxVelocityVector(), FxAngle3D())
   with Structure {
 
   private val originalPos: Double = pos.y
-  private var state: Int = DOOR_INITIAL_STATE
-  private var counter: Double = 0
+  private var state: DoorStates = InitialState
+  var counter: Double = 0
 
   def speedUp: Double
 
@@ -33,10 +32,8 @@ abstract class AbstractMovingDoor(world: FxWorld, pos: FxPoint3D, agl: FxAngle3D
   }
 
   override def handleCollisionWith(obj: FxObject, dt: Double): Boolean = {
-    if (state == DOOR_INITIAL_STATE) {
-      // Start move up
-      FxWorld.debug(this, "open")
-      state = DOOR_START_MOVING_UP
+    if (state == InitialState) {
+      state = StartMovingUp
     }
     false
   }
@@ -45,13 +42,12 @@ abstract class AbstractMovingDoor(world: FxWorld, pos: FxPoint3D, agl: FxAngle3D
     super.update(dt)
 
     state match {
-      case DOOR_INITIAL_STATE =>
-      case DOOR_START_MOVING_UP => doorStartMovingUp(dt)
-      case DOOR_MOVING_UP => doorMovingUp(dt)
-      case DOOR_WAITING => doorWait(dt)
-      case DOOR_MOVING_DOWN => doorMovingDown(dt)
+      case InitialState =>
+      case StartMovingUp => doorStartMovingUp(dt)
+      case MovingUp => doorMovingUp(dt)
+      case Waiting => doorWait(dt)
+      case MovingDown => doorMovingDown(dt)
       case _ =>
-        FxWorld.debug(this, s"Door state ($state) is unrecognized")
     }
   }
 
@@ -59,7 +55,7 @@ abstract class AbstractMovingDoor(world: FxWorld, pos: FxPoint3D, agl: FxAngle3D
     val p = getdPosition()
     p.y = speedUp * dt
     setdPosition(p)
-    state = DOOR_MOVING_UP
+    state = MovingUp
   }
 
   private def doorMovingUp(dt: Double) {
@@ -68,7 +64,7 @@ abstract class AbstractMovingDoor(world: FxWorld, pos: FxPoint3D, agl: FxAngle3D
       val dp = getdPosition()
       dp.y = 0
       setdPosition(dp)
-      state = DOOR_WAITING
+      state = Waiting
       counter = 0
     }
   }
@@ -79,7 +75,7 @@ abstract class AbstractMovingDoor(world: FxWorld, pos: FxPoint3D, agl: FxAngle3D
       val p = getdPosition()
       p.y = -speedDown * dt
       setdPosition(p)
-      state = DOOR_MOVING_DOWN
+      state = MovingDown
     }
   }
 
@@ -89,21 +85,8 @@ abstract class AbstractMovingDoor(world: FxWorld, pos: FxPoint3D, agl: FxAngle3D
       val dp = getdPosition()
       dp.y = 0
       setdPosition(dp)
-      state = DOOR_START_MOVING_UP
+      state = StartMovingUp
     }
   }
-
-}
-
-/**
- * Abstract Door
- * @author lawrence.daniels@gmail.com
- */
-object AbstractMovingDoor {
-  val DOOR_INITIAL_STATE = 0
-  val DOOR_START_MOVING_UP = 1
-  val DOOR_MOVING_UP = 2
-  val DOOR_WAITING = 3
-  val DOOR_MOVING_DOWN = 4
 
 }
