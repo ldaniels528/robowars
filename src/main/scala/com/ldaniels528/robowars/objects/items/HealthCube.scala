5d3595faf7419bc61c977e265fea0ef3f8cb7053
ldaniels <lawrence.daniels@topcareife.com>
2014-08-10 11:12:30 -0700

    modified to use the RewardItem trait

--- src/main/scala/com/ldaniels528/robowars/objects/items/HealthCube.scala
+++ src/main/scala/com/ldaniels528/robowars/objects/items/HealthCube.scala
@@ -8,10 +8,20 @@ import com.ldaniels528.robowars.objects.items.HealthCube._
  * Represents a Health Cube
  * @author lawrence.daniels@gmail.com
  */
-case class HealthCube(w: FxWorld, p: FxPoint3D) extends AbstractRewardItem(w, FxPoint3D(p.x, p.y + SCALE.h, p.z)) {
+case class HealthCube(w: FxWorld, p: FxPoint3D)
+  extends FxMovingObject(w, FxPoint3D(p.x, p.y + SCALE.h, p.z), FxAngle3D(), FxVelocityVector(), FxAngle3D())
+  with RewardItem {
 
   // set the default polyhedron instance
   lazy val modelInstance = FxModelInstance("/models/items/cube.f3d", SCALE)
+
+  override def update(dt: Double) {
+    // the item must continually spin
+    spin(dt)
+
+    // allow super-class to update
+    super.update(dt)
+  }
 
 }
 
