e57eef7e54b2cf5501c89b26231630869560020f
Lawrence Daniels <lawrence.daniels@gmail.com>
2014-08-17 11:19:28 -0700

    removed dependency on NetworkPeer for implementing classes

--- src/main/scala/com/ldaniels528/robowars/net/ClientSideProcessor.scala
+++ src/main/scala/com/ldaniels528/robowars/net/ClientSideProcessor.scala
@@ -4,19 +4,21 @@ import akka.actor.{Actor, ActorSystem, Cancellable, Props}
 import com.ldaniels528.robowars.VirtualWorld
 import com.ldaniels528.robowars.net.ClientSideProcessor._
 import com.ldaniels528.robowars.net.NetworkActionProcessor._
-import org.slf4j.LoggerFactory
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future, Promise}
 
 /**
- * RoboWars Client-Side Processor
+ * Client-Side Network Action Processor
  * @author lawrence.daniels@gmail.com
  */
 trait ClientSideProcessor {
 
-  def client: NetworkPeer
-
+  /**
+   * Retrieves a virtual world from a remote peer
+   * @param client the given remote peer
+   * @return the virtual world
+   */
   def getRemoteWorld(client: NetworkPeer): Future[VirtualWorld] = {
     clientSide ! JoinRequest(client, level = 1)
     makePromise(client.id)
@@ -25,7 +27,7 @@ trait ClientSideProcessor {
   /**
    * Sets up a task to receive network actions
    */
-  def scheduleUpdates(interval: FiniteDuration = 5.millis)(implicit ec: ExecutionContext): Cancellable = {
+  def scheduleUpdates(client: NetworkPeer, interval: FiniteDuration = 5.millis)(implicit ec: ExecutionContext): Cancellable = {
     system.scheduler.schedule(initialDelay = 0.millis, interval, new Runnable {
       override def run() {
         // fill the peer's data buffer
@@ -39,14 +41,21 @@ trait ClientSideProcessor {
 
 }
 
+/**
+ * Client-Side Network Action Processor Companion Object
+ * @author lawrence.daniels@gmail.com
+ */
 object ClientSideProcessor {
-  private val logger = LoggerFactory.getLogger(getClass)
-
   // create the client side actor
   private val system = ActorSystem("ClientSideActors")
-  private val clientSide = system.actorOf(Props[ClientSideActor], name = "clientSide")
+  private val clientSide = system.actorOf(Props[ClientSideActor], name = "clientSideActor")
   private val callbacks = collection.concurrent.TrieMap[Long, Promise[VirtualWorld]]()
 
+  /**
+   * Returns a future representing a promise of a virtual world
+   * @param id the given peer ID
+   * @return a future representing a promise of a virtual world
+   */
   def makePromise(id: Long): Future[VirtualWorld] = {
     val promise = Promise[VirtualWorld]()
     callbacks += (id -> promise)
