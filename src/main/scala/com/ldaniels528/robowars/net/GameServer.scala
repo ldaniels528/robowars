b2cd79b66e62c5b7290b66a45e7a1149faee991d
Lawrence Daniels <lawrence.daniels@gmail.com>
2014-08-16 19:00:04 -0700

    completely rewrote the game server; removing the actors and simplifying the implementation.

--- src/main/scala/com/ldaniels528/robowars/net/GameServer.scala
+++ src/main/scala/com/ldaniels528/robowars/net/GameServer.scala
@@ -2,9 +2,9 @@ package com.ldaniels528.robowars.net
 
 import java.net.{ServerSocket, Socket}
 
-import akka.actor.{Actor, ActorSystem, Props}
-import com.ldaniels528.robowars.VirtualWorldReader
-import com.ldaniels528.robowars.net.NetworkActionProcessor.{ShutdownServer, WelcomeRequest, WorldResponse, _}
+import com.ldaniels528.robowars.net.GameServer._
+import com.ldaniels528.robowars.net.NetworkActionProcessor._
+import com.ldaniels528.robowars.{ContentManager, VirtualWorld}
 import org.slf4j.LoggerFactory
 
 import scala.util.Try
@@ -13,35 +13,27 @@ import scala.util.Try
  * RoboWars Game Server
  * @author lawrence.daniels@gmail.com
  */
-object GameServer {
-  private val logger = LoggerFactory.getLogger(getClass)
+class GameServer(port: Int) {
   var alive: Boolean = true
-  private val system = ActorSystem("RemoteClients")
   private val clientMgr = new ClientManager()
-  private var level = 1
+  private var world: VirtualWorld = _
+  private var level: Int = 1
 
-  // create the client side actor
-  private val serverSide = system.actorOf(Props[ServerSideActor], name = "serverSide")
+  def run() {
+    // load the virtual world
+    logger.info(s"Loading virtual world for level $level...")
+    world = ContentManager.loadWorld(f"/worlds/world_$level%04d.xml")
 
-  // create the logging actor
-  private val log = system.actorOf(Props[LoggingActor], name = "logger")
-
-  // start the thread manager
-  new Thread(clientMgr).start()
-
-  /**
-   * Main application entry-point
-   * @param args the given command line arguments
-   */
-  def main(args: Array[String]) {
-    // get the listen port
-    val port = args.headOption map (_.toInt) getOrElse 8855
+    // start the thread manager
+    logger.info("Starting client manager...")
+    new Thread(clientMgr).start()
 
     // start listening on the port
     logger.info(s"Binding to port $port...")
     val serverSock = new ServerSocket(port)
     logger.info(s"Listening on port $port")
 
+    // loop indefinitely
     while (alive) {
       // get the client connection
       val socket = serverSock.accept()
@@ -56,10 +48,10 @@ object GameServer {
    * Client Connection Manager
    * @author lawrence.daniels@gmail.com
    */
-  class ClientManager() extends Runnable {
+  class ClientManager() extends Runnable with NetworkActionTransmission {
     // define the client container
     var clients: List[Client] = Nil
-    val bytes = new Array[Byte](65536)
+    val bytes = new Array[Byte](8192)
 
     def +=(socket: Socket) {
       // create a client wrapper
@@ -74,53 +66,55 @@ object GameServer {
         // check for input from each client
         clients foreach (client => manage(client))
 
-        // sleep 5 millis
-        Thread.sleep(5)
+        // sleep 1 millis
+        Thread.sleep(1)
       }
     }
 
-    private def manage(client: Client) {
+    private def manage(peer: Client) {
       Try {
-        if (client.in.available() > 0) {
+        if (peer.in.available() > 0) {
           // write the chunk of data to the client's buffer
-          val count = client.in.read(bytes)
-          log ! s"Read $count bytes for client ${client.socket.getInetAddress.getHostName}"
-          client.buffer.put(bytes, 0, count)
+          val count = peer.in.read(bytes)
+          logger.info(s"Read $count bytes for client ${peer.socket.getInetAddress.getHostName}")
+          peer.buffer.put(bytes, 0, count)
         }
 
         // attempt to decode messages from the client
-        if(client.buffer.nonEmpty) {
-          decodeNext(client) foreach (serverSide ! _)
+        if (peer.buffer.nonEmpty) {
+          decodeNext(peer) foreach {
+            case HelloRequest(client) => send(client, HelloResponse(client, availableSlots = 4))
+            case r@HelloResponse(client, _) => send(client, r)
+            case WorldRequest(client, _) => send(client, WorldResponse(client, world))
+            case r@WorldResponse(client, _) => send(client, r)
+            case unknown =>
+              logger.error(s"Unhandled message $unknown (${unknown.getClass.getName}})")
+          }
         }
       }
     }
   }
 
-  /**
-   * Asynchronous logging actor
-   * @author lawrence.daniels@gmail.com
-   */
-  class LoggingActor() extends Actor {
-    def receive = {
-      case message: String => logger.info(message)
-      case unknown => this.unhandled(unknown)
-    }
-  }
+}
+
+/**
+ * RoboWars Game Server
+ * @author lawrence.daniels@gmail.com
+ */
+object GameServer {
+  private val logger = LoggerFactory.getLogger(getClass)
 
   /**
-   * Server-Side Client Handler Actor
-   * @author lawrence.daniels@gmail.com
+   * Main application entry-point
+   * @param args the given command line arguments
    */
-  class ServerSideActor() extends Actor with Transmitter {
-    def receive = {
-      case ShutdownServer(client) =>
-      case WelcomeRequest(client) => send(client, WelcomeResponse(client, availableSlots = 4))
-      case r @ WelcomeResponse(client, _) => send(client, r)
-      case WorldRequest(client, _) => send(client, WorldResponse(client, VirtualWorldReader.load(f"/worlds/world_$level%04d.xml")))
-      case r @ WorldResponse(client, _) => send(client, r)
-      case unknown => this.unhandled(unknown)
-    }
+  def main(args: Array[String]) {
+    // get the listen port
+    val port = args.headOption map (_.toInt) getOrElse 8855
 
+    // start the server
+    val server = new GameServer(port)
+    server.run()
   }
 
 }
